44c9846900db1de4c53c11ae217568825a3781fd
digi-monkey <digimonkey@protonmail.com>
2023-12-22 18:24:40 +0800

    wip: return cache from rocks db

--- src/db.rs
+++ src/db.rs
@@ -6,25 +6,32 @@ pub fn open_db() -> DB {
     return db;
 }
 
-pub fn test_db() {
-    // Open a RocksDB database
-    let path = "./rocksdb";
-    let db = DB::open_default(path).expect("Failed to open RocksDB");
+#[cfg(test)]
+mod test {
+    // Import the testing module
+    use super::*;
 
-    // Write data to RocksDB
-    let key = "example_key";
-    let value = "example_value";
-    db.put(key.as_bytes(), value.as_bytes())
-        .expect("Failed to write to RocksDB");
+    #[test]
+    pub fn test_db() {
+        // Open a RocksDB database
+        let path = "./rocksdb";
+        let db = DB::open_default(path).expect("Failed to open RocksDB");
 
-    // Read data from RocksDB
-    let read_result = db.get(key.as_bytes());
-    match read_result {
-        Ok(Some(value)) => {
-            let value_str = String::from_utf8_lossy(&value);
-            println!("Read from RocksDB: {}", value_str);
+        // Write data to RocksDB
+        let key = "example_key";
+        let value = "example_value";
+        db.put(key.as_bytes(), value.as_bytes())
+            .expect("Failed to write to RocksDB");
+
+        // Read data from RocksDB
+        let read_result = db.get(key.as_bytes());
+        match read_result {
+            Ok(Some(value)) => {
+                let value_str = String::from_utf8_lossy(&value);
+                println!("Read from RocksDB: {}", value_str);
+            }
+            Ok(None) => println!("Key not found in RocksDB"),
+            Err(err) => eprintln!("Error reading from RocksDB: {:?}", err),
         }
-        Ok(None) => println!("Key not found in RocksDB"),
-        Err(err) => eprintln!("Error reading from RocksDB: {:?}", err),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,46 +11,81 @@ use relays::RELAY_URLS;
 use std::{convert::Infallible, env};
 use warp::Filter;
 
+use crate::metadata::Meta;
+
 const BECH32_SK: &str = "nsec1ufnus6pju578ste3v90xd5m2decpuzpql2295m3sknqcjzyys9ls0qlc85";
+
+// todo: reuse &client/db to save connections
+async fn get_event(event_id: String) -> Result<impl warp::Reply, Infallible> {
+    let secret_key = SecretKey::from_bech32(BECH32_SK).expect("no key");
+    let my_keys = Keys::new(secret_key);
+    let db = open_db();
+    let read_result = db.get(event_id.clone());
+    match read_result {
+        Ok(Some(value)) => {
+            let value_str = String::from_utf8_lossy(&value).to_string();
+            let v = value_str.clone();
+            let json_val: Result<Meta, serde_json::Error> = serde_json::from_str(v.as_str());
+            match json_val {
+                Ok(res) => {
+                    println!("hit cache for RocksDB: {:#?}", value_str);
+                    return Ok(warp::reply::json(
+                        &serde_json::json!({"status": "ok", "data": res}),
+                    ));
+                }
+                Err(err) => {
+                    println!("decode cache err: {:#?}", err);
+                    return Ok(warp::reply::json(&serde_json::json!({"status": "failed"})));
+                }
+            };
+        }
+        Ok(None) => println!("Key not found in RocksDB"),
+        Err(err) => eprintln!("Error reading from RocksDB: {:?}", err),
+    }
+
+    let client = Client::new(&my_keys);
+    let opt = RelayOptions::new().write(false);
+    for relay in RELAY_URLS {
+        client
+            .add_relay_with_opts(relay, None, opt.clone())
+            .await
+            .expect("");
+    }
+
+    client.connect().await;
+    println!("conencted!");
+
+    let instance = Nostr::new(&client);
+    let event = instance
+        .get_event_by_id(EventId::from_hex(event_id.clone()).expect(""))
+        .await;
+
+    let key = event_id.clone();
+    match event {
+        Ok(event) => {
+            let handle = Metadata::new(&event);
+            let data = handle.to_meta();
+            let json_str_data = &serde_json::to_string(&data);
+            match json_str_data {
+                Ok(res) => {
+                    db.put(key.as_bytes(), res)
+                        .expect("Failed to write to RocksDB");
+                }
+                Err(_err) => {}
+            }
+
+            Ok(warp::reply::json(
+                &serde_json::json!({"status": "ok", "data": data}),
+            ))
+        }
+        Err(_err) => Ok(warp::reply::json(
+            &serde_json::json!({"status": "not found"}),
+        )),
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let db = open_db();
-    // todo: reuse &client to save connections
-    async fn get_event(event_id: String) -> Result<impl warp::Reply, Infallible> {
-        let secret_key = SecretKey::from_bech32(BECH32_SK).expect("no key");
-        let my_keys = Keys::new(secret_key);
-
-        let client = Client::new(&my_keys);
-        let opt = RelayOptions::new().write(false);
-        for relay in RELAY_URLS {
-            client
-                .add_relay_with_opts(relay, None, opt.clone())
-                .await
-                .expect("");
-        }
-
-        client.connect().await;
-        println!("conencted!");
-
-        let instance = Nostr::new(&client);
-        let event = instance
-            .get_event_by_id(EventId::from_hex(event_id).expect(""))
-            .await;
-
-        match event {
-            Ok(event) => {
-                let handle = Metadata::new(&event);
-                Ok(warp::reply::json(
-                    &serde_json::json!({"status": "ok", "data": handle.to_meta()}),
-                ))
-            }
-            Err(_err) => Ok(warp::reply::json(
-                &serde_json::json!({"status": "not found"}),
-            )),
-        }
-    }
-
     let default_port = 8080;
     let port: u16 = env::var("PORT")
         .ok()
